73aaa183972d0aae63da5138c138b1af177e5b8c
ahnemui <clh.oneil@gmail.com>
2021-05-20 15:10:49 +1000

    testing ranking board on test.rb

--- test.rb
+++ test.rb
@@ -1,7 +1,28 @@
-require "tty-spinner"
-spinner = TTY::Spinner.new(frames: ["1", "2", "3", "4", "5", "6"], clear: true)
-spinner.auto_spin
-sleep 1.5
-spinner.stop
-puts rand(1..6)
+scoreboard = {
+    "test1" => 100,
+    "test2" => -300,
+    "test3" => 700,
+    "test4" => 800,
+    "test5" => 7900,
+    "test6" => 1000,
+}
+
+scoreboard.store("test7", 700)
+
+ranking = scoreboard.sort_by {|key, value| value}.reverse.to_h
+
+i = 1
+ranking.each do |key, value|
+    case i
+    when 1
+        puts "1st: #{key}, #{value}pts"
+    when 2
+        puts "2nd: #{key}, #{value}pts"
+    when 3
+        puts "3rd: #{key}, #{value}pts"
+    else
+        puts "#{i}th: #{key}, #{value}pts"
+    end
+    i += 1
+end
 
