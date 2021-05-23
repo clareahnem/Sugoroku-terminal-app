f3e6ef1f572242ba1a4320613c9a8f6ee34fdac4
ahnemui <clh.oneil@gmail.com>
2021-05-23 14:27:12 +1000

    added ranking.txt to save user data

--- play_sugoroku.rb
+++ play_sugoroku.rb
@@ -1,13 +1,30 @@
 require_relative "board"
 
+scores_hash = {
+    "test1" => 100,
+    "test2" => -300,
+    "test3" => 700,
+    "test4" => 800,
+    "test5" => 7900,
+    "test6" => 1000,
+}
+
 class Play
     attr_accessor :name
     attr_reader :position, :score 
+    # scores_hash = {
+    #         "test1" => 100,
+    #         "test2" => -300,
+    #         "test3" => 700,
+    #         "test4" => 800,
+    #         "test5" => 7900,
+    #         "test6" => 1000,
+    #         }
+
     def initialize(name)
         @name = name
         @position = 0
         @score = 0
-        # @board = SugorokuBoard.new
     end
 
     
@@ -18,24 +35,6 @@ class Play
         end
         sleep(1)
         landed_on_which_panel(@position)
-        # case @position
-        # when  3, 6, 11, 18, 21, 26, 27, 32
-        #     puts "Hooray! You have landed on a green panel! You gained 300pts!".colorize(:green)
-        #     @score += 300
-        # when  4, 14, 17, 20, 23, 29
-        #     puts "Oh no! You have landed on a red panel! You lost 100pts".colorize(:red)
-        #     @score -= 100
-        # when  9, 16, 31
-        #     puts "You've landed on a blue panel! Roll the dice to multiply your score!".colorize(:blue)
-        #     bonus_panel
-        # else
-        #     if position < 32
-        #         puts "You've landed on a standard panel. You gained 100pts!"
-        #         @score += 100
-        #     else
-        #         puts "Congratulations! You've reached the Goal!".colorize(:magenta)
-        #     end
-        # end
     end
 
     def landed_on_which_panel(position)
@@ -55,6 +54,7 @@ class Play
                 @score += 100
             else
                 puts "Congratulations! You've reached the Goal!".colorize(:magenta)
+                save_data(@name, @score)
             end
         end
     end
@@ -68,6 +68,20 @@ class Play
     def display_score
         @board.display_position_on_board(@position)
     end
+
+    #test ID 13
+    def save_data(name, score)
+        prompt = TTY::Prompt.new
+        save_output = prompt.select("Would you like to submit your score to ranking board?", ["Yes", "No"])
+        if save_output == "Yes"
+            File.write('ranking.txt', "#{name} #{score} \n", File.size('ranking.txt'), mode: 'a')
+            sleep(0.5)
+            puts "Your score has been successfully submitted!"
+        end
+
+    end
+    
+
 end
 
 # clare = Play.new("clare")
--- test.rb
+++ test.rb
@@ -1,3 +1,5 @@
+require "tty-table"
+
 scoreboard = {
     "test1" => 100,
     "test2" => -300,
@@ -9,20 +11,37 @@ scoreboard = {
 
 scoreboard.store("test7", 700)
 
-ranking = scoreboard.sort_by {|key, value| value}.reverse.to_h
+ranking = scoreboard.sort_by {|key, value| value}.reverse
 
+pp ranking
+
+# table = TTY::Table.new(["Rank", "Name", "Score"], ["a", "b", "c"])
 i = 1
-ranking.each do |key, value|
-    case i
-    when 1
-        puts "1st: #{key}, #{value}pts"
-    when 2
-        puts "2nd: #{key}, #{value}pts"
-    when 3
-        puts "3rd: #{key}, #{value}pts"
-    else
-        puts "#{i}th: #{key}, #{value}pts"
-    end
+ranking.each do |one_data|
+    one_data.unshift i
     i += 1
 end
 
+pp ranking
+
+# puts table.render(:acsii)
+
+
+# i = 1
+# ranking.each do |key, value|
+#     case i
+#     when 1
+#         puts "1st: #{key}, #{value}pts"
+#     when 2
+#         puts "2nd: #{key}, #{value}pts"
+#     when 3
+#         puts "3rd: #{key}, #{value}pts"
+#     else
+#         puts "#{i}th: #{key}, #{value}pts"
+#     end
+#     i += 1
+# end
+
+#teat ID 12-2
+table = TTY::Table.new(["Rank","Name","Score"], ranking)
+puts table.render(:ascii)
