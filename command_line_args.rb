3f7684e8bfa630631a7f138bdd45fedc9e9c1d09
ahnemui <clh.oneil@gmail.com>
2021-05-24 12:46:02 +1000

    made changes to flags in command_line_args.rb

--- command_line_args.rb
+++ command_line_args.rb
@@ -1,25 +1,26 @@
+# test ID 19
 if ARGV.length > 0
     flag, *rest = ARGV
     ARGV.clear
     case flag
-    when '-help'
+    when '-help', '--h'
         system 'clear'
-        puts "This is a Terminal Sugoroku Game Application."
+        puts "\nThis is a Terminal Sugoroku Game Application."
         puts "To load Program, simply run the Terminal_Sugoroku.sh file by typing in './Terminal_Sugoroku.sh' on your terminal."
-        puts "make sure that you are in the correct directory(folder) when typing the above command"
+        puts "PleaseMake sure that you are in the correct directory(folder) when typing the above command"
         puts "This will lead you to the main page with options to read instructions, see top scores and playing Sugoroku Game."
-        puts "\nFor more information about this app, see 'ruby index.rb -info' or go to https://github.com/clareahnem/Sugoroku-terminal-app and read instructions listed on README.md "
+        puts "For more information about this app, see 'ruby index.rb -info'/'ruby index.rb --i' or go to https://github.com/clareahnem/Sugoroku-terminal-app and read instructions listed on README.md "
         exit
-    when '-info'
+    when '-info', '--i'
         puts "This application is running on  Ruby Version: #{RUBY_VERSION}"
         puts "Before starting the Game, you will need to download the Ruby Gem Bundler for this application, which is listed on this app's GITHUB page"
         puts "Please go to https://github.com/clareahnem/Sugoroku-terminal-app and read through README.md on more detailed instructions on how to install ruby gems required for this Application"
         exit
     else
         puts "invalid argument."
-        puts "for help, please type in'ruby index.rb -help"
-        puts "for information, please type in'ruby index.rb -info"
-        puts "for further instructions, please refer to README.md on https://github.com/clareahnem/Sugoroku-terminal-app. This will contain detailed instructions on how to run this application."
+        puts "For help, please type in'ruby index.rb -help' or 'ruby index.rb --h'"
+        puts "For information, please type in'ruby index.rb -info' or 'ruby index.rb --i'"
+        puts "For further instructions, please refer to README.md on https://github.com/clareahnem/Sugoroku-terminal-app. This will contain detailed instructions on how to run this application."
         exit
     end
 end
